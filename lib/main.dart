e99406a8ac1f241237b6fc38d9d423a92702a258
matev0syan <matevoosyann@gmail.com>
2022-03-23 00:37:40 +0400

    first change

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
 
+void main(List<String> args) => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        backgroundColor: Colors.white,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
